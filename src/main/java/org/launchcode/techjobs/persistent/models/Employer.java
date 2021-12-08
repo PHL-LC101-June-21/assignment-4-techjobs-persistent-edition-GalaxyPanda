2890038eebd83201fc5a019557df0e98bb97b8d9
GalaxyPanda <kasikfranks@gmail.com>
2021-12-08 01:05:34 -0500

    Finished Part 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,31 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @NotNull
+    @Size(min=3, max=100, message = "Location is required")
+    private String location;
+
+
+
+    public Employer(){}
+
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
